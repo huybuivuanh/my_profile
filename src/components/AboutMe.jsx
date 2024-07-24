65c579e6a0f4a27f3e566021f2332fe282b25aa6
Bùi Vũ Anh Huy <buivuanhhuy@Buis-MacBook-Pro.local>
2024-07-23 18:06:17 -0600

    added to about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -12,7 +12,16 @@ function AboutMe() {
         university in 2021 and have enjoyed learning programming here.
         Throughout the years, I've developed a wide range of technical and soft
         skills. I'm currently seeking an internship to enhance my skills and
-        gain work experience.
+        gain work experience.<br></br>
+        <br></br>I moved to Canada from Vietnam in 2013, beginning my studies
+        here in grade 6. My passion for math and physics developed early on,
+        complemented by a love for music. After graduating high school in 2020,
+        I decided to take a gap year to work and save money for university.
+        During this time, I worked at my parents' restaurant in Prince Albert.
+        This experience not only helped me financially but also taught me
+        valuable lessons in responsibility and hard work. In 2021, I began my
+        university journey, eager to explore the fields of mobile and web app
+        development.
       </p>
     </div>
   );
